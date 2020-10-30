fe0d2fe47e3f85f77cd2e6a276c95f1532dbef9c
felix2056 <felixdaniel2056@gmail.com>
2020-10-30 22:51:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<?php include_once('includes/header.php'); ?>
+
+<?php include_once('includes/nav.php'); ?>
+
+<div class="page-container drawer-page-content" id="PageContainer">
+    <main class="main-content js-focus-hidden" id="MainContent" role="main" tabindex="-1">
+        <!-- BEGIN content_for_index -->
+        <div id="shopify-section-hero-1" class="shopify-section index-section index-section--flush">
+            <div data-section-id="hero-1" data-section-type="hero-section">
+                <div class="hero hero--large hero-hero-1 hero__overlay box ratio-container js lazyloaded"
+                    id="Hero-hero-1" data-layout="full_width"
+                    data-bg="//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_300x300.jpg?v=1603732263" data-bgset="//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_180x.jpg?v=1603732263 180w 120h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_360x.jpg?v=1603732263 360w 240h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_540x.jpg?v=1603732263 540w 360h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_720x.jpg?v=1603732263 720w 480h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_900x.jpg?v=1603732263 900w 600h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1080x.jpg?v=1603732263 1080w 720h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1296x.jpg?v=1603732263 1296w 864h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1512x.jpg?v=1603732263 1512w 1008h,
+                    //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1728x.jpg?v=1603732263 1728w 1152h,
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1.jpg?v=1603732263 1800w 1200h" data-parent-fit="cover"
+                    style="background-position: center center; background-image: url(&quot;https://cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1512x.jpg?v=1603732263&quot;);"
+                    alt="" src="//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_300x300.jpg?v=1603732263">
+                    <div class="hero__inner">
+                        <div class="page-width text-center">
+                            <h2 class="h1 mega-title mega-title--large">Early Christmas And New Year Sale</h2>
+                            <div class="rte-setting mega-subtitle mega-subtitle--large">
+                                <p>This is a clearance sale and therefore only limited products are available.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <picture style="display: none;">
+                        <source
+                            data-srcset="//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_180x.jpg?v=1603732263 180w 120h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_360x.jpg?v=1603732263 360w 240h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_540x.jpg?v=1603732263 540w 360h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_720x.jpg?v=1603732263 720w 480h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_900x.jpg?v=1603732263 900w 600h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1080x.jpg?v=1603732263 1080w 720h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1296x.jpg?v=1603732263 1296w 864h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1512x.jpg?v=1603732263 1512w 1008h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1728x.jpg?v=1603732263 1728w 1152h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1.jpg?v=1603732263 1800w 1200h"
+                            sizes="1349px"
+                            srcset="//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_180x.jpg?v=1603732263 180w 120h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_360x.jpg?v=1603732263 360w 240h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_540x.jpg?v=1603732263 540w 360h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_720x.jpg?v=1603732263 720w 480h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_900x.jpg?v=1603732263 900w 600h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1080x.jpg?v=1603732263 1080w 720h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1296x.jpg?v=1603732263 1296w 864h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1512x.jpg?v=1603732263 1512w 1008h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_1728x.jpg?v=1603732263 1728w 1152h, //cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1.jpg?v=1603732263 1800w 1200h">
+                        <img alt="" class="lazyautosizes lazyloaded" data-sizes="auto" data-parent-fit="cover"
+                            sizes="1349px">
+                    </picture>
+                </div>
+                <noscript>
+                    <div class="hero hero--large hero__overlay"
+                        style="background-image: url('//cdn.shopify.com/s/files/1/0506/7145/2322/files/fdl1_2048x.jpg?v=1603732263'); background-position: center center;">
+                    </div>
+                </noscript>
+            </div>
+
+
+        </div>
+        <div id="shopify-section-collection" class="shopify-section index-section">
+            <div class="page-width">
+
+                <div class="section-header text-center">
+                    <h2>Featured collection</h2>
+                </div>
+
+
+                <ul class="grid grid--uniform grid--view-items">
+
+                    <li class="grid__item grid__item--collection small--one-half medium-up--one-quarter">
+
+                        <div class="grid-view-item product-card">
+                            <a class="grid-view-item__link grid-view-item__image-container full-width-link"
+                                href="/products/single.php?product=11">
+                                <span class="visually-hidden">Boeing CH-47 Chinook RC Helicopter</span>
+                            </a>
+
+
+
+
+                            <style>
+                            #ProductCardImage-collection-5864249229474 {
+                                max-width: 396.19651347068145px;
+                                max-height: 250px;
+                            }
+
+                            #ProductCardImageWrapper-collection-5864249229474 {
+                                max-width: 396.19651347068145px;
+                            }
+                            </style>
+
+
+
+                            <div class="product-card__image-with-placeholder-wrapper">
+                                <div id="ProductCardImageWrapper-collection-5864249229474"
+                                    class="grid-view-item__image-wrapper product-card__image-wrapper js">
+                                    <div style="padding-top:63.1%;">
+                                        <img id="ProductCardImage-collection-5864249229474"
+                                            class="grid-view-item__image lazyautosizes ls-is-cached lazyloaded" alt=""
+                                            data-widths="[180, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]"
+                                            data-aspectratio="1.5847860538827259" data-sizes="auto" data-image=""
+                                            data-srcset="//cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_180x.png?v=1603728356 180w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_360x.png?v=1603728356 360w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_540x.png?v=1603728356 540w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_720x.png?v=1603728356 720w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_900x.png?v=1603728356 900w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1080x.png?v=1603728356 1080w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1296x.png?v=1603728356 1296w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1512x.png?v=1603728356 1512w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1728x.png?v=1603728356 1728w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_2048x.png?v=1603728356 2048w"
+                                            sizes="250px"
+                                            srcset="//cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_180x.png?v=1603728356 180w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_360x.png?v=1603728356 360w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_540x.png?v=1603728356 540w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_720x.png?v=1603728356 720w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_900x.png?v=1603728356 900w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1080x.png?v=1603728356 1080w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1296x.png?v=1603728356 1296w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1512x.png?v=1603728356 1512w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_1728x.png?v=1603728356 1728w, //cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_2048x.png?v=1603728356 2048w">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <noscript>
+
+                                <img class="grid-view-item__image"
+                                    src="//cdn.shopify.com/s/files/1/0506/7145/2322/products/57d63e6c5cf64e23ca868a135f14503f_250x250@2x.png?v=1603728356"
+                                    alt="" style="max-width: 396.19651347068145px;">
+                            </noscript>
+
+                            <div class="h4 grid-view-item__title product-card__title" aria-hidden="true">Boeing CH-47
+                                Chinook RC
+                                Helicopter</div>
+
+
+                            <dl class="price price--listing price--on-sale ">
+
+                                <div class="price__regular">
+                                    <dt>
+                                        <span class="visually-hidden visually-hidden--inline">Regular price</span>
+                                    </dt>
+                                    <dd>
+                                        <span class="price-item price-item--regular"><span
+                                                class="money notranslate">$36.50</span>
+                                        </span>
+                                    </dd>
+                                </div>
+                                <div class="price__sale">
+                                    <dt>
+                                        <span class="visually-hidden visually-hidden--inline">Sale price</span>
+                                    </dt>
+                                    <dd>
+                                        <span class="price-item price-item--sale"><span
+                                                class="money notranslate">$22.10</span>
+                                        </span>
+                                    </dd>
+                                    <div class="price__compare">
+                                        <dt>
+                                            <span class="visually-hidden visually-hidden--inline">Regular price</span>
+                                        </dt>
+                                        <dd>
+                                            <s class="price-item price-item--regular">
+                                                <span class="money notranslate">$36.50</span>
+                                            </s>
+                                        </dd>
+                                    </div>
+                                </div>
+                                <div class="price__unit">
+                                    <dt>
+                                        <span class="visually-hidden visually-hidden--inline">Unit price</span>
+                                    </dt>
+                                    <dd class="price-unit-price"><span></span><span aria-hidden="true">/</span><span
+                                            class="visually-hidden">per&nbsp;</span><span></span></dd>
+                                </div>
+                                <div class="price__badges price__badges--listing">
+                                    <span class="price__badge price__badge--sale" aria-hidden="true">
+                                        <span>Sale</span>
+                                    </span>
+                                    <span class="price__badge price__badge--sold-out">
+                                        <span>Sold out</span>
+                                    </span>
+                                </div>
+                            </dl>
+
+
+                        </div>
+
+                    </li>
+
+                </ul>
+
+
+
+            </div>
+
+
+
+
+        </div><!-- END content_for_index -->
+
+    </main>
+
+    <?php include_once('includes/modals.php'); ?>
+
+    <div id="slideshow-info" class="visually-hidden" aria-hidden="true">Use left/right arrows to navigate the slideshow
+        or swipe left/right if using a mobile device</div>
+
+</div>
+
+  <?php include_once('includes/modals.php'); ?>
+</body>
